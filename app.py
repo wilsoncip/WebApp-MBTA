230347523f2dcb6187f68c201b60d5b5f778d121
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-11-07 16:30:41 -0500

    update

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ from mbta_helper import find_stop_near
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/station/", methods=["GET", "POST"])
 def station():
     if request.method == "POST":
@@ -22,9 +26,9 @@ def station():
                 place=place,
                 nearest_station=nearest_station
             )
-        else:
-            return render_template("form.html", error=True)
-    return render_template("form.html", error=None)
+        # else:
+    #         return render_template("form.html", error=True)
+    # return render_template("form.html", error=None)
 
 
 
