0580d4362192619838af2062e6a326b9f250ac70
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-11-07 16:59:08 -0500

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -30,8 +30,6 @@ def get_lat_long(location):
     """
     Given a place name or address, return a (latitude, longitude) tuple
     with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding  API URL formatting requirements.
     """
     APIKEY = MAPQUEST_API_KEY
     data = get_json(f'http://www.mapquestapi.com/geocoding/v1/address?key={APIKEY}&location={location}')
@@ -43,8 +41,6 @@ def get_nearest_station(latitude, longitude):
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
     tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
     """
     import urllib.request
     import json
@@ -54,9 +50,13 @@ def get_nearest_station(latitude, longitude):
     response_text = request.read().decode('utf-8')
     response_data = json.loads(response_text)
     
-    station_name = response_data['data'][0]['attributes']['name']
-    wheelchair_accessible = response_data['data'][0]['attributes']['wheelchair_boarding']
-    return f"You are closest to {station_name}. Wheelchair status: {wheelchair_accessible}"
+    dict = {}
+    dict['station_name'] = response_data['data'][0]['attributes']['name']
+    
+    wheelchair = {0: 'No Information', 1: 'Accessible', 2: "Inaccessible"}
+    dict['wheelchair_accessible'] = wheelchair[response_data['data'][0]['attributes']['wheelchair_boarding']]
+    
+    return dict
 
 
 def find_stop_near(place_name):
@@ -66,7 +66,6 @@ def find_stop_near(place_name):
     current_location = get_lat_long(place_name)
     nearest = get_nearest_station(current_location[0], current_location[1])
     return nearest
-
 
 
 
