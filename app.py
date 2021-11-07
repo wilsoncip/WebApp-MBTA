dfcb75e9ba2c09f00fb8e657dbd4fad2d2dc4609
vincentiusvictor <vvictor1@babson.edu>
2021-11-07 16:14:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,30 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
+
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/station/", methods=["GET", "POST"])
+def station():
+    if request.method == "POST":
+        place = request.form["place"]
+        nearest_station = find_stop_near(place)
+
+        if nearest_station:
+            return render_template(
+                "result.html",
+                place=place,
+                nearest_station=nearest_station
+            )
+        else:
+            return render_template("form.html", error=True)
+    return render_template("form.html", error=None)
+
 
 
 if __name__ == '__main__':
