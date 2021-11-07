0ceb2f773d0792ed7733429452c3217f30693e1b
wilsoncip <89822808+wilsoncip@users.noreply.github.com>
2021-11-07 17:32:39 -0500

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("main_page.html")
 
 @app.route("/station/", methods=["GET", "POST"])
 def station():
